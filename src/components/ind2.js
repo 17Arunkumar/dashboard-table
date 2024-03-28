ba90d0256e7030f151042c652452f257cc83ded7
17Arunkumar <arunkumarabde17@.com>
2024-03-28 08:46:43 +0530

    zcjgfy

---
+++ src/components/ind2.js
@@ -0,0 +1,1 @@
+export { default as AreaTable } from "./dashboard/areaTable/AreaTable";
---
+++ src/screen/dashboard/DashboardScreen.jsx
@@ -0,0 +1,13 @@
+import { AreaTable } from "../../components/ind2";
+
+
+
+const Dashboard = () => {
+  return (
+    <div className="content-area">
+      <AreaTable/>
+    </div>
+  );
+};
+
+export default Dashboard;
